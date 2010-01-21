a6cf58b49536efad550a927dbd69306fbafead0e
Thibaud Morel l'Horset <teebes@gmail.com>
2010-01-21 18:53:11 -0500

    various django admin tweaks, especially to Mobs & Mob Loaders

--- apps/anima/admin.py
+++ apps/anima/admin.py
@@ -4,9 +4,19 @@ from stark.apps.anima.models import Player, Mob, MobLoader
 class PlayerAdmin(admin.ModelAdmin): pass
 class MobAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'template', 'room')
+    list_editable = ('room',)
 class MobLoaderAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'template_mob')
     exclude = ('spawned_mobs',)
+    filter_horizontal = ('armor', 'weapon', 'misc', 'sustenance', 'spawn_in')
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "template_mob":
+            kwargs["queryset"] = Mob.objects.filter(template=True)
+            return db_field.formfield(**kwargs)
+        return super(MobLoaderAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+    
+    
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(Mob, MobAdmin)
 admin.site.register(MobLoader, MobLoaderAdmin)
